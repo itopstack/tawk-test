0f2d591f2a682231470dda5109c227ddc876d311
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-09-08 18:20:02 +0700

    Update users data source when fetch successfully

--- TawkTest/Screens/UserList/UserListViewControllerViewModel.swift
+++ TawkTest/Screens/UserList/UserListViewControllerViewModel.swift
@@ -7,22 +7,37 @@
 
 import Foundation
 
+protocol UserListViewControllerViewModelDelegate: AnyObject {
+    func userListViewControllerViewModelDidFetchUsersSuccessfully(_ viewModel: UserListViewControllerViewModel)
+    func userListViewControllerViewModelDidFetchUsersFail(_ viewModel: UserListViewControllerViewModel, errorMessage: String)
+}
+
 final class UserListViewControllerViewModel {
     private let service: UsersFetchable
+    private weak var delegate: UserListViewControllerViewModelDelegate?
     
     private(set) var userId = 0
+    private(set) var error: Error?
+    private(set) var users: [GithubUser] = []
     
-    init(service: UsersFetchable = GithubService()) {
+    init(service: UsersFetchable = GithubService(), delegate: UserListViewControllerViewModelDelegate) {
         self.service = service
+        self.delegate = delegate
     }
     
     func fetchUsers() {
         service.fetchUsers(since: userId) { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             case let .success(users):
-                self?.userId += 1
+                self.userId += 1
+                self.users.append(contentsOf: users)
+                self.delegate?.userListViewControllerViewModelDidFetchUsersSuccessfully(self)
+                
             case let .failure(error):
-                break
+                self.error = error
+                self.delegate?.userListViewControllerViewModelDidFetchUsersFail(self, errorMessage: error.localizedDescription)
             }
         }
     }
--- TawkTestTests/Screens/UserList/UserListViewControllerViewModelTests.swift
+++ TawkTestTests/Screens/UserList/UserListViewControllerViewModelTests.swift
@@ -11,39 +11,49 @@ import XCTest
 class UserListViewControllerViewModelTests: XCTestCase {
     private var sut: UserListViewControllerViewModel!
     private var mockService: MockGithubService!
+    private var mockDelegate: MockUserListViewControllerViewModelDelegate!
     
     override func setUp() {
         super.setUp()
         
         mockService = MockGithubService()
-        sut = UserListViewControllerViewModel(service: mockService)
+        mockDelegate = MockUserListViewControllerViewModelDelegate()
+        sut = UserListViewControllerViewModel(service: mockService, delegate: mockDelegate)
     }
     
     override func tearDown() {
         super.tearDown()
         
+        mockDelegate = nil
         mockService = nil
         sut = nil
     }
     
     func test_initialValues() {
         XCTAssertEqual(sut.userId, 0)
+        XCTAssertNil(sut.error)
+        XCTAssertTrue(sut.users.isEmpty)
     }
     
-    func test_userId_increaseIfFetchSuccessfully() {
+    func test_fetchUsers_successfully() {
         mockService.testCase = .success
         
         sut.fetchUsers()
         
         XCTAssertEqual(sut.userId, 1)
+        XCTAssertEqual(sut.users.count, 1)
+        XCTAssertIdentical(mockDelegate.args1.first, sut)
     }
     
-    func test_userId_sameIfFetchFail() {
+    func test_fetchUsers_fail() {
         mockService.testCase = .failure
         
         sut.fetchUsers()
         
         XCTAssertEqual(sut.userId, 0)
+        XCTAssertNotNil(sut.error)
+        XCTAssertIdentical(mockDelegate.args2.first?.0, sut)
+        XCTAssertEqual(mockDelegate.args2.first?.1, anyError.localizedDescription)
     }
 }
 
@@ -60,11 +70,24 @@ final class MockGithubService: UsersFetchable {
     func fetchUsers(since: Int, completion: @escaping (Result<[GithubUser], Error>) -> Void) {
         switch testCase {
         case .success:
-            completion(.success([]))
+            completion(.success([uniqueUser()]))
         case .failure:
             completion(.failure(anyError))
         default:
             fatalError("Test case is missing")
         }
+    }
+}
+
+final class MockUserListViewControllerViewModelDelegate: UserListViewControllerViewModelDelegate {
+    
+    private(set) var args1: [UserListViewControllerViewModel] = []
+    func userListViewControllerViewModelDidFetchUsersSuccessfully(_ viewModel: UserListViewControllerViewModel) {
+        args1.append(viewModel)
+    }
+    
+    private(set) var args2: [(UserListViewControllerViewModel, String)] = []
+    func userListViewControllerViewModelDidFetchUsersFail(_ viewModel: UserListViewControllerViewModel, errorMessage: String) {
+        args2.append((viewModel, errorMessage))
     }
 }
