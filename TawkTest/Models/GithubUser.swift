a164dc273e21cdecd02f23dfa5e1d4d35ee87ec9
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-09-11 01:14:31 +0700

    Change GithubUser from struct to class

--- TawkTest/Models/GithubUser.swift
+++ TawkTest/Models/GithubUser.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct GithubUser: Equatable {
+final class GithubUser: Equatable {
     let login: String
     let id: Int
     let avatarUrl: String
@@ -21,7 +21,46 @@ struct GithubUser: Equatable {
     var hasSeen = false
     var note: String? = nil
     
+    init(login: String,
+         id: Int,
+         avatarUrl: String,
+         url: String,
+         followersUrl: String,
+         organizationsUrl: String,
+         reposUrl: String,
+         type: String,
+         siteAdmin: Bool,
+         hasSeen: Bool = false,
+         note: String? = nil) {
+        
+        self.login = login
+        self.id = id
+        self.avatarUrl = avatarUrl
+        self.url = url
+        self.followersUrl = followersUrl
+        self.organizationsUrl = organizationsUrl
+        self.reposUrl = reposUrl
+        self.type = type
+        self.siteAdmin = siteAdmin
+        self.hasSeen = hasSeen
+        self.note = note
+    }
+    
     static let `default` = GithubUser(login: "", id: 0, avatarUrl: "", url: "", followersUrl: "", organizationsUrl: "", reposUrl: "", type: "", siteAdmin: false)
+    
+    static func == (lhs: GithubUser, rhs: GithubUser) -> Bool {
+        lhs.login == rhs.login &&
+        lhs.id == rhs.id &&
+        lhs.avatarUrl == rhs.avatarUrl &&
+        lhs.url == rhs.url &&
+        lhs.followersUrl == rhs.followersUrl &&
+        lhs.organizationsUrl == rhs.organizationsUrl &&
+        lhs.reposUrl == rhs.reposUrl &&
+        lhs.type == rhs.type &&
+        lhs.siteAdmin == rhs.siteAdmin &&
+        lhs.hasSeen == rhs.hasSeen &&
+        lhs.note == rhs.note
+    }
 }
 
 extension GithubUser: Codable {
