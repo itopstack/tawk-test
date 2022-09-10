11e958bf0cb925fe34278d1635188a24f7859e6f
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-09-10 18:09:08 +0700

    Add image downloader tests

--- TawkTestTests/Services/ImageDownloaderTests.swift
+++ TawkTestTests/Services/ImageDownloaderTests.swift
@@ -9,5 +9,74 @@ import XCTest
 @testable import TawkTest
 
 final class ImageDownloaderTests: XCTestCase {
+    private var sut: ImageDownloader!
+    private var mockSession: MockURLSession!
+    
+    override func setUp() {
+        super.setUp()
+        
+        mockSession = MockURLSession()
+        sut = ImageDownloader(session: mockSession)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        mockSession = nil
+        sut = nil
+    }
+    
+    func test_fetch_fromCached() {
+        let urlString = "https://avatars.githubusercontent.com/u/15520417?v=4"
+        let data = urlString.data(using: .utf8)
+        let exp = expectation(description: "Wait for completion")
+        sut.fetch(from: urlString) { _ in }
+        mockSession.requestArgs.first?.1(data, nil, nil)
+        
+        var cachedData: Data?
+        sut.fetch(from: urlString) { data in
+            cachedData = data
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+        XCTAssertEqual(cachedData, data)
+    }
+    
+    func test_fetch_fromInvalidURL() {
+        let exp = expectation(description: "Wait for completion")
+        
+        var receivedData: Data?
+        sut.fetch(from: "invalid url") { data in
+            receivedData = data
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+        XCTAssertNil(receivedData)
+    }
+    
+    func test_fetch_fromURLSession() {
+        let urlString = "https://avatars.githubusercontent.com/u/15520417?v=4"
+        let data = urlString.data(using: .utf8)
+        let exp = expectation(description: "Wait for completion")
+        
+        var receivedData: Data?
+        sut.fetch(from: urlString) { data in
+            receivedData = data
+            exp.fulfill()
+        }
+        mockSession.requestArgs.first?.1(data, nil, nil)
+        
+        waitForExpectations(timeout: 1)
+        
+        XCTAssertEqual(receivedData, data)
+    }
+}
 
+private final class MockURLSession: Requestable {
+    private(set) var requestArgs: [(URL, (Data?, URLResponse?, Error?) -> Void)] = []
+    func request(with url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        requestArgs.append((url, completion))
+    }
 }
